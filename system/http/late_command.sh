9e07d22e11c38167b9c590d0bfcabb6ff7f0ef1e
Kaiting Chen <kaitingc@vmware.com>
2019-12-04 10:28:21 -0800

    Remove unused late_command.sh

--- system/http/late_command.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash -e
-
-# Allow sudo for overseer user without password or tty
-cat > "/etc/sudoers.d/overseer" <<EOF
-Defaults:overseer !requiretty
-overseer ALL=(ALL) NOPASSWD: ALL
-EOF
-chmod 440 "/etc/sudoers.d/overseer"
