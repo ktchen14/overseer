f82b17dcf2af8bb3aefcd7a8733faec7380f015b
Kaiting Chen <ktchen14@gmail.com>
2020-01-03 10:52:32 -0800

    Add camera

--- vision/layout.c
+++ vision/layout.c
@@ -2,6 +2,10 @@
 
 #include "device.h"
 
+static vs_device_t CAMERA = {
+  .name = "camera", .view_list = { "DualScreen", "Screen1", NULL },
+};
+
 static vs_device_t GPU_VIDEO_1 = {
   .name = "GPU_VIDEO_1", .view_list = { "DualScreen", "Screen1", NULL },
   .xtra = "<rom file=\"/home/ktchen14/rom\" />",
@@ -40,5 +44,6 @@ vs_device_t *vs_device_list[] = {
   &GPU_VIDEO_1, &GPU_AUDIO_1,
   &GPU_VIDEO_2, &GPU_AUDIO_2,
   &SWITCH_PORT_1, &SWITCH_PORT_2, &SWITCH_PORT_3, &SWITCH_PORT_4,
+  &CAMERA,
   NULL,
 };
