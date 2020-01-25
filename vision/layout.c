99c5977df9e8924f4e04d06f3d9d2bd9b814541e
Kaiting Chen <kaitingc@vmware.com>
2020-01-25 15:05:31 -0800

    Add a device for each USB port

--- vision/layout.c
+++ vision/layout.c
@@ -20,8 +20,24 @@ static vs_device_t GPU2_AUDIO = {
   .name = "GPU2_AUDIO", .view_list = { "DualScreen", "Screen2", NULL },
 };
 
-static vs_device_t SWITCH_PORT_1 = {
-  .name = "SWITCH_PORT_1", .view_list = { "DualScreen", "Screen1", NULL },
+// static vs_device_t SWITCH_PORT_1 = {
+//   .name = "SWITCH_PORT_1", .view_list = { "DualScreen", "Screen1", NULL },
+// };
+
+static vs_device_t USBHUB1_1 = {
+  .name = "USBHUB1_1", .view_list = { "DualScreen", "Screen1", NULL },
+};
+static vs_device_t USBHUB1_2 = {
+  .name = "USBHUB1_2", .view_list = { "DualScreen", "Screen1", NULL },
+};
+static vs_device_t USBHUB1_3 = {
+  .name = "USBHUB1_3", .view_list = { "DualScreen", "Screen1", NULL },
+};
+static vs_device_t USBHUB1_4 = {
+  .name = "USBHUB1_4", .view_list = { "DualScreen", "Screen1", NULL },
+};
+static vs_device_t USBHUB1_5 = {
+  .name = "USBHUB1_5", .view_list = { "DualScreen", "Screen1", NULL },
 };
 
 static vs_device_t SWITCH_PORT_2 = {
@@ -32,13 +48,31 @@ static vs_device_t SWITCH_PORT_3 = {
   .name = "SWITCH_PORT_3", .view_list = { "DualScreen", NULL },
 };
 
-static vs_device_t SWITCH_PORT_4 = {
-  .name = "SWITCH_PORT_4", .view_list = { "DualScreen", "Screen2", NULL },
+static vs_device_t USBHUB2_1 = {
+  .name = "USBHUB2_1", .view_list = { "DualScreen", "Screen2", NULL },
 };
+static vs_device_t USBHUB2_2 = {
+  .name = "USBHUB2_2", .view_list = { "DualScreen", "Screen2", NULL },
+};
+static vs_device_t USBHUB2_3 = {
+  .name = "USBHUB2_3", .view_list = { "DualScreen", "Screen2", NULL },
+};
+static vs_device_t USBHUB2_4 = {
+  .name = "USBHUB2_4", .view_list = { "DualScreen", "Screen2", NULL },
+};
+static vs_device_t USBHUB2_5 = {
+  .name = "USBHUB2_5", .view_list = { "DualScreen", "Screen2", NULL },
+};
+
+// static vs_device_t SWITCH_PORT_4 = {
+//   .name = "SWITCH_PORT_4", .view_list = { "DualScreen", "Screen2", NULL },
+// };
 
 vs_device_t *vs_device_list[] = {
   &GPU1_VIDEO, &GPU1_AUDIO,
   &GPU2_VIDEO, &GPU2_AUDIO,
-  &SWITCH_PORT_1, &SWITCH_PORT_2, &SWITCH_PORT_3, &SWITCH_PORT_4,
+  &USBHUB1_1, &USBHUB1_2, &USBHUB1_3, &USBHUB1_4, &USBHUB1_5,
+  &SWITCH_PORT_2, &SWITCH_PORT_3,
+  &USBHUB2_1, &USBHUB2_2, &USBHUB2_3, &USBHUB2_4, &USBHUB2_5,
   NULL,
 };
